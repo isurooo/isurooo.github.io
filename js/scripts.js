6f4e2bcccf4a8429c1f36cbd4ed9a4c7fae856af
Isuru Rodrigo <isurooo@users.noreply.github.com>
2024-06-11 20:44:55 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
-// update1
+// update2
 // Scripts 
 // 
 
